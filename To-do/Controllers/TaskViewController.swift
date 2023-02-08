1e2206d595020ce6e25f5ad0b2619b50a92312f4
baris dilekci <bdilekci200@gmail.com>
2023-02-08 17:06:53 +0300

    Added collectionview to list tasks on the Task screen.

--- To-do/Controllers/TaskViewController.swift
+++ To-do/Controllers/TaskViewController.swift
@@ -5,57 +5,105 @@
 //  Created by Baris on 6.02.2023.
 //
 
+
 import UIKit
 
-class TaskViewController: UIViewController {
 
+private let reuseIdentifier = "TaskCell"
+class TaskViewController: UIViewController {
+    
+    //MARK: - Properties
+ 
+   // private var tasks = [Task]()
     //MARK: - UI Elements
     private lazy var newTaskButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(systemName: "plus"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "plus."), for: .normal)
         button.contentHorizontalAlignment = .fill
         button.contentVerticalAlignment = .fill
         button.tintColor = .white
-        button.addTarget(self, action: #selector(handleNewTaskButton), for: .touchUpInside)
         return button
     }()
     
+    private let collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        return collectionView
+    }()
+    
+    private let nameLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.textColor = .white
+        label.text = " "
+        return label
+    }()
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
         style()
         layout()
-
-        // Do any additional setup after loading the view.
-
+        
+        
     }
+    
 }
 
-//MARK: - Selector
-extension TaskViewController {
-    @objc private func handleNewTaskButton() {
-        let controller = NewTaskViewController()
-        if let sheet = controller.sheetPresentationController {
-            sheet.detents = [.medium()]
-        }
-        self.present(controller, animated: true)
-    }
-}
-//MARK: - Helpers
-extension TaskViewController {
-    private func style() {
+
+// MARK: - Helpers
+extension TaskViewController{
+    private func style(){
         backgroundGradientColor()
+        self.navigationController?.navigationBar.isHidden = true
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.navigationController?.navigationBar.isHidden = true
+        newTaskButton.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .lightGray
         
     }
-    private func layout() {
+    private func layout(){
+        view.addSubview(collectionView)
         view.addSubview(newTaskButton)
-        NSLayoutConstraint.activate([
-            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: newTaskButton.bottomAnchor,constant: 35),
-            view.trailingAnchor.constraint(equalTo: newTaskButton.trailingAnchor,constant: 10),
-            newTaskButton.heightAnchor.constraint(equalToConstant: 60),
-            newTaskButton.widthAnchor.constraint(equalToConstant: 60)
-        ])
+        view.addSubview(nameLabel)
         
+        
+        
+        NSLayoutConstraint.activate([
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: newTaskButton.bottomAnchor, constant: 30),
+            view.trailingAnchor.constraint(equalTo: newTaskButton.trailingAnchor, constant: 4),
+            newTaskButton.heightAnchor.constraint(equalToConstant: 60),
+            newTaskButton.widthAnchor.constraint(equalToConstant: 60),
+            
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            
+            collectionView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 14)
+        ])
     }
+    
+
 }
+
+//MARK: - CollectionView
+extension TaskViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+}
+
